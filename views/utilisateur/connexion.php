c9a1e4eb7b04f9af17e27319aef341511ed29eba
Jaune-Senat <tenarindia@gmail.com>
2026-01-28 14:52:36 +0100

    inscription

---
+++ views/utilisateur/connexion.php
@@ -0,0 +1,15 @@
+<h1>Connexion</h1>
+
+<form action="" method="post">
+    <p>
+        <label>Email :</label>
+        <input type="text" name="email" maxlength="255" cols="100%" placeholder="Entre ton email">
+    </p>
+    <p>
+        <label>Mot de passe :</label>
+        <input type="text"  name="mdp" maxlength="255" cols="100%" placeholder="Entre ton mot de passe">
+    </p>
+    <p>
+        <input type="submit" class="" name="submit" value="Se connecter">
+    </p>
+</form>
---
+++ views/utilisateur/inscription.php
@@ -0,0 +1,25 @@
+<form action="" method="post">
+    <p>
+        <label for="">Nom :</label>
+        <input type="text" name="nom" placeholder="Indiquez le nom">
+    </p>
+    <p>
+        <label for="">Prénom :</label>
+        <input type="text" name="prenom" placeholder="Indiquez le prénom">
+    </p>
+    <p>
+        <label for="">Mot de passe :</label>
+        <input type="text" name="password" placeholder="Indiquez le mot de passe">
+    </p>
+    <p>
+        <label for="">Email :</label>
+        <input type="text" name="email" placeholder="Indiquez l'email">
+    </p>
+    <p>
+        <label for="">Répéter mot de passe :</label>
+        <input type="text" name="password_repeat" placeholder="Répétez le mot de passe">
+    </p>
+    <p>
+        <input type="submit" name="submit" value="Ajouter l'employé">
+    </p>
+</form>
