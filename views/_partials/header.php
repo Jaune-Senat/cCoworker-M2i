ff005806859b321e1a4b8e9fc9b69b59a4bf3059
Dany Houssin <danyhoussin50@gmail.com>
2026-01-28 14:30:31 +0100

    Header et page de connexion mis en place

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style/app.css">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <title>Titre de la page</title>
 </head>
-<body>
+<body id="wrapper">
     <header>
-        
+        <nav>
+            <img src="public/img/logo-CCoworker.png">
+            
+                <ul>
+                    <a href="#"><li>Planning</li></a>
+                    <a href="#"><li>Espaces</li></a>
+                    <a href="#"><li>Utilisateurs</li></a>
+                </ul>
+                
+
+                <a href="#">
+                    <p>Se connecter</p>
+                </a>
+            
+        </nav>
     </header>
