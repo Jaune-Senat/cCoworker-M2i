090dc8ee4c0fc8be4f4970cb89d87d55796be3ec
Dany Houssin <danyhoussin50@gmail.com>
2026-01-28 11:11:59 +0100

    Ajout formulaire de connexion

--- views/connexion.php
+++ views/connexion.php
@@ -1,1 +1,15 @@
 <h1>Connexion</h1>
+
+<form action="" method="post">
+    <p>
+        <label>Identifiant :</label>
+        <input type="text" name="identifiant" maxlength="255" cols="100%" placeholder="Entre ton identifiant">
+    </p>
+    <p>
+        <label>Mot de passe :</label>
+        <input type="text"  name="mdp" maxlength="255" cols="100%" placeholder="Entre ton mot de passe">
+    </p>
+    <p>
+        <input type="submit" class="" name="submit" value="Se connecter">
+    </p>
+</form>
