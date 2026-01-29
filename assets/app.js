f76d03a8a1f00bb23d767d09db2c6d3795c0e053
Dany Houssin <danyhoussin50@gmail.com>
2026-01-29 16:27:58 +0100

    Mise en place du style pour la page liste, modification et suppression pour les espaces

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,8 @@
 const BOUTONS_SUPPRIMER = Array.from(document.getElementsByClassName("btn-supprimer"));
 const POPUP_SUPPRIMER_ESPACE = document.getElementById("supprimer_espace");
 const POPUP_SUPPRIMER_ESPACE_TITRE = POPUP_SUPPRIMER_ESPACE.getElementsByTagName("h2")[0];
-const POPUP_SUPPRIMER_ESPACE_OUI = POPUP_SUPPRIMER_ESPACE.getElementsByTagName("div")[0];
-const POPUP_SUPPRIMER_ESPACE_NON = POPUP_SUPPRIMER_ESPACE.getElementsByTagName("div")[1];
+const POPUP_SUPPRIMER_ESPACE_OUI = POPUP_SUPPRIMER_ESPACE.getElementsByTagName("span")[0];
+const POPUP_SUPPRIMER_ESPACE_NON = POPUP_SUPPRIMER_ESPACE.getElementsByTagName("span")[1];
 
 let idEspace;
 
@@ -11,7 +11,7 @@ POPUP_SUPPRIMER_ESPACE.style.display = "none";
 
 BOUTONS_SUPPRIMER.forEach(bouton => {
     bouton.addEventListener("click", (e) => {
-        POPUP_SUPPRIMER_ESPACE.style.display = "block";
+        POPUP_SUPPRIMER_ESPACE.style.display = "flex";
         POPUP_SUPPRIMER_ESPACE_TITRE.innerText = "Supprimer l'espace numéro " + bouton.id.substring(17) + " ?";
         idEspace = bouton.id.substring(17);
     });
