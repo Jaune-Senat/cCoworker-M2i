426d01503820d1d75e610cb8228917ddd2c537cb
Jaune-Senat <tenarindia@gmail.com>
2026-01-29 16:24:52 +0100

    front form ajout espace

--- views/_partials/header.php
+++ views/_partials/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style/app.css">
-<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <title>Titre de la page</title>
 </head>
 <body id="wrapper">
@@ -14,7 +14,7 @@
             <?php if (isset($_SESSION["utilisateur"])) {?>
                 <ul>
                     <a href="#"><li>Planning</li></a>
-                    <a href="index.php?controller=espace&action=list"><li>Espaces</li></a>
+                    <a href="#"><li>Espaces</li></a>
                     <a href="#"><li>Utilisateurs</li></a>
                 </ul>
                 <div class="utilisateur">
