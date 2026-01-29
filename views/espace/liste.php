5d6974243f58cb8856e791e09b7b903735a4ee05
Xaphan67 <cedric.falda@gmail.com>
2026-01-29 14:07:52 +0100

    Ajout fonction d'ajout / modification / suppression d'un espace

--- views/espace/liste.php
+++ views/espace/liste.php
@@ -1,3 +1,18 @@
 <h1>Liste des espaces</h1>
 
 <a href="index.php?controller=espace&action=add">Ajouter un espace</a>
+
+<?php foreach ($espaces as $espace) { ?>
+    <p>
+        Espace numéro : <?=  $espace["num_espace"] ?> - Capacité : <?=  $espace["capacite_espace"] ?> - Type : <?=  $espace["nom_type"] ?>
+        - <a href="index.php?controller=espace&action=edit&id=<?=  $espace["id_espace"] ?>">Modifer</a>
+        <span id="supprimer-espace-<?=  $espace["num_espace"] ?>" class="btn-supprimer">Supprimer</span></p>
+<?php } ?>
+
+<!-- Ceci est un popup qui devra s'afficher quand on clique sur le bouton supprimer correspondant à un espace précis -->
+<div id="supprimer_espace" class="popup">
+    <h2>Supprimer l'espace numéro</h2>
+    <p>Êtes-vous sûr de vouloir supprimer cet espace ?</p>
+    <div>Oui</div>
+    <div>Non</div>
+</div>
