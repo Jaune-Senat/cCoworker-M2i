e119dbed14b1aec5cb3db4a8467afaa1aeba2e58
Xaphan67 <cedric.falda@gmail.com>
2026-02-02 10:16:25 +0100

    Ajout vue modification d'une réservation

---
+++ views/reservation/modifier.php
@@ -0,0 +1,31 @@
+<h1>Modifier une réservation</h1>
+
+<form action="" method="post">
+    <div>
+        <label for="nom">Nom client :</label>
+        <input type="text" name="nom" placeholder="Nom du client" value="<?= $reservation["nom_reservation"] ?>" required>
+    </div>
+    <div>
+        <label for="prenom">Prenom client :</label>
+        <input type="text" name="prenom" placeholder="Prenom du client" value="<?= $reservation["prenom_reservation"] ?>" required>
+    </div>
+    <div>
+        <label for="debut">Date de début :</label>
+        <input type="date" name="debut" required> à <input type="time" name="debut-heure" step="1800" required>
+    </div>
+    <div>
+        <label for="fin">Date de fin :</label>
+        <input type="date" name="fin" required> à <input type="time" name="fin-heure" step="1800" required>
+    </div>
+    <div>
+        <label for="espace">Espace</label>
+        <Select name="espace" required>
+            <?php foreach($espaces as $espace){ ?>
+                <option value="<?=  $espace["id_espace"] ?>"><?=  $espace["num_espace"] == $espace["id_espace"] ? " selected" : "" ?>><?=  $espace["num_espace"] ?></option>
+            <?php } ?>
+        </Select>
+    </div>
+    <div>
+        <input type="submit" name="submit" value="Modifier">
+    </div>
+</form>
