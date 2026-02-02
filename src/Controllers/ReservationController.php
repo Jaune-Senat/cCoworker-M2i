aff792725c1d20f85ab65dd1b9af5f89366f6c38
Xaphan67 <cedric.falda@gmail.com>
2026-02-02 10:15:49 +0100

    Ajout modification d'une réservation

--- src/Controllers/ReservationController.php
+++ src/Controllers/ReservationController.php
@@ -112,4 +112,94 @@ class ReservationController extends Controller {
         // Affiche la vue Ajout réservation
         $this->_display("reservation/ajouter");
     }
+
+    // Page de modification d'une réservation
+    public function edit() {
+
+        // Si l'utilisateur n'est pas connecté
+        if (!isset($_SESSION["utilisateur"])) {
+
+            // Redirige l'utilisateur vers la page de connexion
+            header("Location:index.php");
+        }
+
+        // Récupère la liste des espaces
+        $espaceModel = new EspaceModel;
+        $donneesEspaces = $espaceModel->findAll();
+
+        // Crée un tableau pour gérer les erreurs
+        $erreurs = [];
+
+        // Si le formulaire est soumis
+        if (count($_POST) > 0) {
+
+            // Filtrage des données
+            $nom = trim(filter_input(INPUT_POST, "nom", FILTER_SANITIZE_SPECIAL_CHARS));
+            $prenom = trim(filter_input(INPUT_POST, "prenom", FILTER_SANITIZE_SPECIAL_CHARS));
+            $dateDebut = trim(filter_input(INPUT_POST, "debut", FILTER_SANITIZE_SPECIAL_CHARS));
+            $heureDebut = trim(filter_input(INPUT_POST, "debut-heure", FILTER_SANITIZE_SPECIAL_CHARS));
+            $dateFin = trim(filter_input(INPUT_POST, "fin", FILTER_SANITIZE_SPECIAL_CHARS));
+            $heureFin = trim(filter_input(INPUT_POST, "fin-heure", FILTER_SANITIZE_SPECIAL_CHARS));
+
+            if (!$nom) {
+                $erreurs["nom"] = "Le nom est obligatoire";
+            } 
+            if (!$prenom) {
+                $erreurs["prenom"] = "Le prenom est obligatoire";
+            }
+            if (!$dateDebut) {
+                $erreurs["dateDebut"] = "La date de début est obligatoire";
+            }
+            if (!$heureDebut) {
+                $erreurs["heureDebut"] = "L'heure de début est obligatoire";
+            }
+            if (!$dateFin) {
+                $erreurs["dateFin"] = "La date de fin est obligatoire";
+            } 
+            else if ($dateDebut > $dateFin) {
+                $erreurs["dateDebutInvalide"] = "La date de début doit précéder celle de fin est obligatoire";
+            }
+            if (!$heureFin) {
+                $erreurs["heureFin"] = "L'heure de fin est obligatoire";
+            }
+
+            // Récupère la réservation en base de données
+            $reservationModel = new ReservationModel;
+            $donnesReservation = $reservationModel->findById($_GET["id"]);
+
+            // Si pas d'erreur
+            if (count($erreurs) == 0) {
+
+                // Crée un objet Réservation et l'hydrate
+                $objReservation = new Reservation;
+                $objReservation->hydrate($donnesReservation);
+
+                // Modifie la réservation
+                $objReservation->setNom_client($nom);
+                $objReservation->setPrenom_client($prenom);
+                $objReservation->setDebut($dateDebut . ' ' . $heureDebut . ':00');
+                $objReservation->setFin($dateFin . ' ' . $heureFin . ':00');
+                $objReservation->setEspace($_POST["espace"]);
+
+                // Met à jour la réservation en base de données
+                $boolInsert = $reservationModel->edit($objReservation);
+
+                if (!$boolInsert) {
+                    $erreur["insertion"] = "Une erreur s'est produite lors de l'insertion en base de donnée";
+                } 
+                else {
+                    // Redirige l'utilisateur vers le planning
+                    header("Location:index.php?controller=reservation&action=planning");
+                }
+            }
+        }
+
+        // Indique à la vue les variables nécesaires
+        $this->_donnees["erreurs"] = $erreurs;
+        $this->_donnees["espaces"] = $donneesEspaces;
+        $this->_donnees["reservation"] = $donnesReservation;
+
+        // Affiche la vue Ajout réservation
+        $this->_display("reservation/ajouter");
+    }
 }
